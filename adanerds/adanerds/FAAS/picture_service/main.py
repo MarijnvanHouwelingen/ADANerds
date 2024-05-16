82a1c590b2e8bf49a6f1db09b43fd8ea0a43e88b
Cayah99 <kyra.jongman@gmail.com>
2024-05-16 16:55:17 +0200

    added refund and picture service

---
+++ adanerds/adanerds/FAAS/picture_service/main.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, jsonify
+from google.cloud import storage
+import json 
+import base64
+import functions_framework
+from pub_sub.pub_sub import create_topic, publish_message, create_subscription
+
+app = Flask(__name__)
+
+project_id = "" # id of project
+bucket_name = "" # name of bucket
+
+client = storage.Client()
+bucket = client.bucket(bucket_name) # name of bucket
+
+listing_topic_id = "create/update_listing"
+listing_subscription_id = "listing-service-subscription"
+
+create_topic(project_id, listing_topic_id)
+create_subscription(project_id, listing_topic_id, listing_subscription_id)
+
+def publish_listing_event(project_id, listing_topic_id, listing_data): #####, Add information from bounded context, like listing_id, etc): #####
+    '''
+    This function is responsible for publishing listing information from the listing bounded context to the event bus. It should be 
+    integrated into the listing bounded context service to publish listing events whenever a listing is initiated. 
+    (Pub_Listing in Component Diagram)
+    '''
+    listing_event = {
+        "type": "Listing",
+        "data": listing_data ##### Add the variable which are initalized in the function here (see listing_doa.py) #####
+    }
+    publish_message(project_id, listing_topic_id, listing_event) # Pub_Listing
+    print(f"Published listing event for listing ID {listing_data}")
+
+@functions_framework.cloud_event
+def handle_listing_event(cloud_event):
+    '''
+    This function is designed to subscribe to the create/update_listing topic and process listing information from the event bus. 
+    It will be triggered whenever a new message is published to the create/update_listing topic.
+    (Sub_Listing in Component Diagram)
+    '''
+    data = cloud_event.data
+    message_data = json.loads(base64.b64decode(data["message"]["data"]).decode('utf-8')) #Sub_Listing
+    print(f'Message data: {message_data}')
+
+    if message_data['type'] == "Listing":
+        ##### Get the same information that is stored in the event bus
+        return "", 200
+
+    return jsonify({'error': 'We apologize, but we are unable to issue a refund at this time.'}), 400
+
+@app.route('/pictures', methods=['POST', 'GET'])
+def pictures():
+    '''
+    This function is designed to POST and GET images from a specified google cloud bucket
+    '''
+    if request.method == 'POST':
+        picture = request.files.get('file')
+        if picture:
+            blob = bucket.blob(picture.filename)
+            blob.upload_from_string(picture.read(), content_type=picture.content_type)
+            return jsonify({"message": f"Uploaded {picture.filename} to {bucket.name}"}), 200
+        return jsonify({"error": "No file uploaded"}), 400
+
+    if request.method == 'GET':
+        filename = request.args.get('filename')
+        if filename:
+            blob = bucket.blob(filename)
+            url = blob.generate_signed_url(3600, version="v4") 
+            return jsonify({"url": url}), 200
+        return jsonify({"error": "No filename provided"}), 400
---
+++ adanerds/adanerds/FAAS/refund_service/main.py
@@ -0,0 +1,67 @@
+import json 
+import base64
+import functions_framework
+from pub_sub.pub_sub import create_topic, publish_message, create_subscription
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+project_id = "" # id of project
+booking_topic_id = "booking_refund"
+refund_subscription_id = "refund-service-subscription"
+refund_status_topic_id = "refund_status"
+
+create_topic(project_id, booking_topic_id)
+create_subscription(project_id, booking_topic_id, refund_subscription_id)
+create_topic(project_id, refund_status_topic_id)
+
+def publish_refund_event(project_id, booking_topic_id, booking_id, refund_amount): 
+    '''
+    This function is responsible for publishing refund information from the booking bounded context to the event bus. It should be 
+    integrated into the booking bounded context service to publish refund events whenever a refund is initiated. 
+    (Pub_Booking in Component Diagram)
+    '''
+    refund_event = {
+        "type": "Refund",
+        "data": {
+            "booking_id": booking_id,
+            "refund_amount": refund_amount
+        }
+    }
+    publish_message(project_id, booking_topic_id, refund_event)
+    print(f"Published refund event for booking ID {booking_id}")
+
+@functions_framework.cloud_event
+def handle_refund_event(cloud_event):
+    '''
+    This function is designed to subscribe to the booking-refunds topic and process refund information from the event bus. 
+    It will be triggered whenever a new message is published to the booking-refunds topic and publish the refund status 
+    to the refund-status topic.
+    (Sub_Refund and Pub_Refund in Component Diagram)
+    '''
+    data = cloud_event.data
+    message_data = json.loads(base64.b64decode(data["message"]["data"]).decode('utf-8')) #Sub_Refund
+    print(f'Message data: {message_data}')
+
+    if message_data['type'] == "Refund":
+        booking_id = message_data['data']['booking_id']
+        refund_amount = message_data['data']['refund_amount']
+        print(f"Processing refund of ${refund_amount} for booking ID {booking_id}")
+        
+        refund_status = {
+            "type": "RefundStatus",
+            "data": {
+                "booking_id": booking_id,
+                "status": "Processed",
+                "refund_amount": refund_amount
+            }
+        }
+        publish_message(project_id, refund_status_topic_id, refund_status) # Pub_Refund
+        print(f"Published refund status for booking ID {booking_id}")
+        return jsonify({'message': f'Refund of ${refund_amount} for booking ID {booking_id} has been processed'}), 200
+
+    return jsonify({'error': 'We apologize, but we are unable to issue a refund at this time.'}), 400
+
+
+
+
