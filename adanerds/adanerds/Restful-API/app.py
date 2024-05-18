ac466862afee3d1548da7cb4b22ec8c10dd612ae
Mark van Vlierden <m.vanvlierden@tilburguniversity.edu>
2024-05-18 17:55:57 +0200

    Added function in our Restful-api that can check if a user is authorized based on their request header by sending header to authentication service

--- adanerds/adanerds/Restful-API/app.py
+++ adanerds/adanerds/Restful-API/app.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import requests
 
 import connexion
 from connexion.resolver import RestyResolver
@@ -15,6 +16,24 @@ app.add_api('account-api.yaml')
 
 # Initialize CORS
 CORS(app.app)
+
+
+def check_if_authorize(req):
+    auth_header = req.headers['Authorization']
+    if 'AUTH_URL' in os.environ:
+        auth_url = os.environ['AUTH_URL']
+    else:
+        auth_url = 'http://authentication_service_ct:5000/verify'
+    result = requests.post(auth_url,
+                           headers={'Content-Type': 'application/json',
+                                    'Authorization': auth_header})
+    status_code = result.status_code
+    
+    logging.info(f"Authentication service returned status code: {status_code}")
+    logging.info(f"Authentication service returned output: {result.json()}")
+
+    return status_code
+
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
